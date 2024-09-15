8e220e4f072588e8e09ae579ef76b45aa3997490
1deme <demetrelabadze1@gmail.com>
2024-09-15 18:30:17 +0400

    added cycle detection

--- src/dnf/Conjunction.java
+++ src/dnf/Conjunction.java
@@ -1,12 +1,17 @@
 package dnf;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import constraintElements.Element;
 import predicates.PrimitiveConstraint;
 import transformations.Sim;
 import transformations.Unif;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 
 public class Conjunction {
@@ -24,20 +29,46 @@ public class Conjunction {
     public boolean sim(){
         return Sim.sim(conjunction);
     }
-    
-    @Override
-    public String toString() {
-        StringBuilder result = new StringBuilder();
-        Iterator<PrimitiveConstraint> iterator = conjunction.iterator();
 
-        while (iterator.hasNext()) {
-            result.append(iterator.next().toString());
-            if (iterator.hasNext()) {
-                result.append(" ^ ");
+    public boolean isElementInCycle(Element startElement) {
+
+        Map<Element, List<Element>> graph = new HashMap<>();
+
+        for (PrimitiveConstraint pc : conjunction) {
+            Element from = pc.el1;
+            Element to = pc.el2;
+            graph.putIfAbsent(from, new ArrayList<>());
+            graph.get(from).add(to);
+        }
+
+        Set<Element> visited = new HashSet<>();
+        Set<Element> recStack = new HashSet<>();
+
+        return hasCycleDFS(startElement, graph, visited, recStack);
+    }
+
+    private boolean hasCycleDFS(Element current, Map<Element, List<Element>> graph, 
+                                Set<Element> visited, Set<Element> recStack) {
+        if (recStack.contains(current)) {
+            return true;  
+        }
+        if (visited.contains(current)) {
+            return false;  
+        }
+
+        visited.add(current);
+        recStack.add(current);
+
+        List<Element> neighbors = graph.get(current);
+        if (neighbors != null) {
+            for (Element neighbor : neighbors) {
+                if (hasCycleDFS(neighbor, graph, visited, recStack)) {
+                    return true;
+                }
             }
         }
 
-        return result.toString();
+        recStack.remove(current);
+        return false;
     }
-    
 }
